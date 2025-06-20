1a1ee5ff958e26af05deed9fee0141ef08b687ed
shreyash gote <goteshreyash50@gmail.com>
2025-06-20 15:30:44 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl ">Welcome to my site </h1>
+      <h1 className="text-5xl text-orange-500 font-bold">Welcome to my site</h1>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
